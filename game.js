e038e60f33b18db0106870e023ed50901f9ca31a
Bianca Power <drbiancampower@gmail.com>
2017-11-12 18:01:56 +1100

    changed all platforms to organic

--- game.js
+++ game.js
@@ -34,14 +34,14 @@ function addPlatforms() {
   platforms = game.add.physicsGroup();
   platforms.create(450, 550, 'platform');
   platforms.create(100, 550, 'platform');
-  platforms.create(100, 100, 'platform2');
+  platforms.create(100, 100, 'platform');
   platforms.create(250, 150, 'platform');
   platforms.create(50, 300, 'platform');
   platforms.create(150, 250, 'platform');
-  platforms.create(650, 300, 'platform2');
+  platforms.create(650, 300, 'platform');
   platforms.create(550, 200, 'platform');
   platforms.create(300, 450, 'platform');
-  platforms.create(400, 350, 'platform2');
+  platforms.create(400, 350, 'platform');
   platforms.setAll('body.immovable', true);
 }
 
@@ -91,8 +91,8 @@ window.onload = function () {
     game.stage.backgroundColor = '#5db1ad';
 
     //Load images
-    game.load.image('platform', 'platform_1.png');
-    game.load.image('platform2', 'platform_2.png');
+    game.load.image('platform', 'platform_2.png');
+    game.load.image('platform2', 'platform_1.png');
 
     //Load spritesheets
     // game.load.spritesheet('player', 'chalkers.png', 48, 62);
